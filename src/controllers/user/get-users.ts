97d9571d202cf8e6eda366543af72dab5cef8d06
shubham homkar <homkar1997@gmail.com>
2025-08-04 13:34:30 +0530

    Fixed API version for get users

--- src/controllers/user/get-users.ts
+++ src/controllers/user/get-users.ts
@@ -3,8 +3,8 @@ import User from "../../models/User";
 
 const getUsers = async (req: Request, res: Response) => {
   try {
-    const loggedInFirebaseId = req.user?.uid;
-    const users = await User.find({ firebaseUserId: { $ne: loggedInFirebaseId } }).select(['name', 'email', 'avatar']);
+    // const loggedInFirebaseId = req.user?.uid;
+    const users = await User.find().select(['name', 'email', 'avatar']);
     return res.status(200).json({ users });
   } catch (error) {
     console.log(error)
